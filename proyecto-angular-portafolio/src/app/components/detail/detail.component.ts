edfa2059b1449eed643d98267e970779810cec54
Luis Hurtado <luis.hurtado@linio.com>
2019-12-13 15:41:44 -0400

    Add detail component and is working now

--- proyecto-angular-portafolio/src/app/components/detail/detail.component.ts
+++ proyecto-angular-portafolio/src/app/components/detail/detail.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Project } from '../../models/project';
 import { ProjectService } from '../../services/project.service';
 import { Global } from '../../services/global';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 
 @Component({
@@ -12,9 +13,37 @@ import { Global } from '../../services/global';
 })
 export class DetailComponent implements OnInit {
 
-  constructor() { }
+  public url: string;
+  public project: Project;
+
+  constructor(
+
+  	private _projectService: ProjectService,
+  	private _router: Router,
+  	private _route: ActivatedRoute
+  	) {
+
+    this.url = Global.url;
+  	 }
 
   ngOnInit() {
+
+  	this._route.params.subscribe(params => {
+      let id = params.id;
+
+      this.getProject(id);
+    })
+  }
+
+  getProject(id){
+    this._projectService.getProject(id).subscribe(
+      response => {
+        this.project = response.project
+      },
+      error => {
+        console.log(<any>error);
+      }
+    )
   }
 
 }
